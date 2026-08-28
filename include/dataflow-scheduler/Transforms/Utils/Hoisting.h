b845ed8d85e69da4e5ee32d5c032209b851db0ce
Karl F. A. Friebel <karl.friebel@ibm.com>
2026-08-28 13:49:03 +0200

    Improved technical quality of hoisting utility.
    - Removed unused function `scheduler::findDominatingWritesIn`.
    
    - Changed `scheduler::getSSADominators` into local utility.
    
      The previous implementation was the result of a much more generic
      walk that did not assume that all operands of the input op already
      satisfy the SSA property. This is unncessary.
    
      The new implementation `findSSAEarliestDominator` is local to the
      file and walks the path to the root exactly once. It also uses the
      fact that the SSA property is known to be satisfied.
    
    - Changed `::visitRestrictedUsersIn` to bail on aliasing.
    
      If any of the visited users is a `mlir::ViewLikeOpInterface`, the
      scan gives up, knowing that there are trivally aliasing values.
    
    Signed-off-by: Karl F. A. Friebel <karl.friebel@ibm.com>

--- include/dataflow-scheduler/Transforms/Utils/Hoisting.h
+++ include/dataflow-scheduler/Transforms/Utils/Hoisting.h
@@ -20,17 +20,9 @@
 #define DATAFLOW_SCHEDULER_TRANSFORMS_UTILS_HOISTING_H_
 
 #include <llvm/ADT/SmallVector.h>
-#include <mlir/IR/Dominance.h>
 #include <mlir/IR/Operation.h>
 
 namespace scheduler {
-
-/// Obtains the smallest set of SSA dominators of @p op .
-///
-/// A region SSA dominates @p op if it or one of its parents defines a value
-/// used by @p op or any of its descendants.
-[[nodiscard]]
-auto getSSADominators(mlir::Operation* op) -> llvm::SmallVector<mlir::Region*>;
 
 /// Finds the topmost operation to which @p op can be hoisted according to SSA.
 [[nodiscard]]
@@ -38,16 +30,6 @@ auto findHoistingTarget(
     mlir::Operation* op,
     llvm::function_ref<bool(mlir::Region*)> should_hoist_out_of)
     -> mlir::Operation*;
-
-/// Finds the smallest set of dominating writes to @p restricted in @p region .
-///
-/// The location @p restricted is assumed to not be aliased by any other value.
-/// If the result is present, it indicates the smallest set of ops writing to
-/// @p restricgted that together dominate all others.
-[[nodiscard]]
-auto findDominatingWritesIn(mlir::Value restricted, mlir::Region* region,
-                            mlir::DominanceInfo& dominance)
-    -> std::optional<llvm::SmallVector<mlir::Operation*>>;
 
 /// Finds the single write to @p restricted in @p region .
 ///
