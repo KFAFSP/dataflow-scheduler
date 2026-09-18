9df3c3140fffa4873dc3b0bf2cd87be7f4c5e708
Karl F. A. Friebel <karl.friebel@ibm.com>
2026-09-18 18:41:21 +0200

    Fixed some `fuse-linalg` bugs.
    - Fixed the issue where the producer-side mapping was not propagated.
    - Fixed the issue with multiple fusion candidates breaking locations.
    
    Signed-off-by: Karl F. A. Friebel <karl.friebel@ibm.com>

--- lib/Conversion/frontend/KTIRToScheduleIR/FuseLinalg.cpp
+++ lib/Conversion/frontend/KTIRToScheduleIR/FuseLinalg.cpp
@@ -19,18 +19,21 @@
 #include <llvm/ADT/ArrayRef.h>
 #include <llvm/ADT/STLExtras.h>
 #include <llvm/ADT/SmallVector.h>
-#include <llvm/Support/Casting.h>
 #include <llvm/Support/CommandLine.h>
-#include <llvm/Support/LogicalResult.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/Dialect/Linalg/IR/Linalg.h>
 #include <mlir/Dialect/Linalg/Transforms/Transforms.h>
 #include <mlir/Dialect/Tensor/IR/Tensor.h>
 #include <mlir/IR/Attributes.h>
+#include <mlir/IR/Builders.h>
+#include <mlir/IR/BuiltinTypeInterfaces.h>
 #include <mlir/IR/Location.h>
 #include <mlir/IR/PatternMatch.h>
+#include <mlir/IR/Value.h>
 #include <mlir/Pass/Pass.h>
 #include <mlir/Transforms/GreedyPatternRewriteDriver.h>
+
+#include <utility>
 
 #include "dataflow-scheduler/Conversion/frontend/KTIRToScheduleIR/Passes.h"  // IWYU pragma: keep
 #include "dataflow-scheduler/Dialect/KTDFArch/KTDFArch.h"
@@ -52,6 +55,139 @@ namespace scheduler {
 }  // namespace scheduler
 
 namespace {
+
+/// Breaks a false dependency through an init operand via 'tensor.empty'.
+///
+/// This is a copy of the (hidden) upstream pattern.
+struct RemoveOutsDependency : mlir::OpRewritePattern<mlir::linalg::GenericOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  auto matchAndRewrite(mlir::linalg::GenericOp generic,
+                       mlir::PatternRewriter& rewriter) const
+      -> llvm::LogicalResult override {
+    const auto loc = generic.getLoc();
+
+    rewriter.startOpModification(generic);
+    bool changed = false;
+    for (auto& init : generic.getDpsInitsMutable()) {
+      const auto type =
+          llvm::dyn_cast<mlir::RankedTensorType>(init.get().getType());
+      if (!generic.payloadUsesValueFromOperand(&init) || !type ||
+          type.getEncoding() ||
+          init.get().getDefiningOp<mlir::tensor::EmptyOp>()) {
+        continue;
+      }
+
+      llvm::SmallVector<mlir::Value> sizes;
+      for (auto [index, size] : llvm::enumerate(type.getShape())) {
+        if (mlir::ShapedType::isDynamic(size)) {
+          sizes.push_back(
+              mlir::tensor::DimOp::create(rewriter, loc, init.get(), index));
+        }
+      }
+      init.set(mlir::tensor::EmptyOp::create(rewriter, loc, type, sizes));
+      changed = true;
+    }
+
+    if (!changed) {
+      rewriter.cancelOpModification(generic);
+      return llvm::failure();
+    }
+    rewriter.finalizeOpModification(generic);
+    return llvm::success();
+  }
+};
+
+/// Determines whether @p lhs and @p rhs are subsets of one another.
+[[nodiscard]] auto setEqual(
+    llvm::MutableArrayRef<mlir::ktdf_arch::ResourceSpecAttr> lhs,
+    llvm::MutableArrayRef<mlir::ktdf_arch::ResourceSpecAttr> rhs) {
+  const auto compare = [](mlir::ktdf_arch::ResourceSpecAttr l,
+                          mlir::ktdf_arch::ResourceSpecAttr r) -> bool {
+    return l.getAsOpaquePointer() < r.getAsOpaquePointer();
+  };
+
+  llvm::sort(lhs, compare);
+  lhs = {lhs.begin(), llvm::unique(lhs)};
+  llvm::sort(rhs, compare);
+  rhs = {rhs.begin(), llvm::unique(rhs)};
+
+  return lhs == rhs;
+}
+
+/// Determines whether the mappings of @p lhs and @p rhs permit fusion.
+[[nodiscard]] auto fuseMappings(mlir::ktdf_arch::MapsToAttr lhs,
+                                mlir::ktdf_arch::MapsToAttr rhs)
+    -> llvm::FailureOr<mlir::ktdf_arch::MapsToAttr> {
+  if (lhs == rhs || !lhs || !rhs) {
+    return lhs ? lhs : rhs;
+  }
+
+  // For now, only permit fusion of ops with the same mapping.
+  llvm::SmallVector<mlir::ktdf_arch::ResourceSpecAttr> buffer;
+  buffer.reserve(lhs.getValue().size() + rhs.getValue().size());
+  llvm::append_range(buffer, lhs.getValue());
+  const auto split = buffer.size();
+  llvm::append_range(buffer, rhs.getValue());
+  if (!setEqual({buffer.data(), buffer.data() + split},
+                {buffer.data() + split, buffer.end()})) {
+    return llvm::failure();
+  }
+
+  // For stability, return the lhs mapping.
+  return lhs;
+}
+
+/// Fuses an elementwise producer into a 'linalg.generic' consumer.
+///
+/// This is a mapping-aware copy of the (hidden) upstream pattern.
+struct FuseElementwiseOps : mlir::OpRewritePattern<mlir::linalg::GenericOp> {
+  FuseElementwiseOps(mlir::MLIRContext* ctx,
+                     mlir::linalg::ControlFusionFn control_fusion)
+      : OpRewritePattern(ctx), control_fusion_(std::move(control_fusion)) {}
+
+  auto matchAndRewrite(mlir::linalg::GenericOp generic,
+                       mlir::PatternRewriter& rewriter) const
+      -> llvm::LogicalResult override {
+    for (auto& through : generic->getOpOperands()) {
+      if (!mlir::linalg::areElementwiseOpsFusable(&through)) {
+        continue;
+      }
+      auto* const producer = through.get().getDefiningOp();
+      const auto fused_mapping =
+          fuseMappings(mlir::ktdf_arch::Mappable::getMapsTo(generic),
+                       mlir::ktdf_arch::Mappable::getMapsTo(producer));
+      if (failed(fused_mapping) || !control_fusion_(&through)) {
+        continue;
+      }
+
+      const auto fusion_result =
+          mlir::linalg::fuseElementwiseOps(rewriter, &through);
+      if (failed(fusion_result)) {
+        return rewriter.notifyMatchFailure(generic, "fusion failed");
+      }
+
+      if (*fused_mapping) {
+        std::ignore = mlir::ktdf_arch::Mappable::setMapsTo(
+            fusion_result->fusedOp, *fused_mapping);
+      }
+
+      for (auto [from, to] : fusion_result->replacements) {
+        rewriter.replaceUsesWithIf(
+            from, to, [&](const mlir::OpOperand& use) -> bool {
+              return use.get().getDefiningOp() != producer;
+            });
+      }
+      rewriter.eraseOp(generic);
+      return llvm::success();
+    }
+
+    return llvm::failure();
+  }
+
+ private:
+  mlir::linalg::ControlFusionFn control_fusion_;
+};
 
 /// Detects the implied mapping of @p generic and explicitly sets it.
 ///
@@ -80,8 +216,7 @@ auto propagateImpliedMapping(mlir::linalg::GenericOp generic)
   }
 
   // There are mapping constraints on the contents of this op that will
-  // have to be respected. Set them on the op, and if that fails, don't
-  // attempt to fuse it.
+  // have to be respected. Try to set them on the op.
   const auto maps_to = mlir::ktdf_arch::MapsToAttr::get(generic->getContext(),
                                                         mapping.getArrayRef());
   if (llvm::succeeded(mappable.setMapsTo(maps_to))) {
@@ -89,38 +224,6 @@ auto propagateImpliedMapping(mlir::linalg::GenericOp generic)
   }
 
   return llvm::failure();
-}
-
-/// Determines whether @p lhs and @p rhs are subsets of one another.
-[[nodiscard]] auto setEqual(
-    llvm::MutableArrayRef<mlir::ktdf_arch::ResourceSpecAttr> lhs,
-    llvm::MutableArrayRef<mlir::ktdf_arch::ResourceSpecAttr> rhs) {
-  const auto compare = [](mlir::ktdf_arch::ResourceSpecAttr l,
-                          mlir::ktdf_arch::ResourceSpecAttr r) -> bool {
-    return l.getAsOpaquePointer() < r.getAsOpaquePointer();
-  };
-
-  llvm::sort(lhs, compare);
-  lhs = {lhs.begin(), llvm::unique(lhs)};
-  llvm::sort(rhs, compare);
-  rhs = {rhs.begin(), llvm::unique(rhs)};
-
-  return lhs == rhs;
-}
-
-[[nodiscard]] auto areCompatible(mlir::ktdf_arch::MapsToAttr lhs,
-                                 mlir::ktdf_arch::MapsToAttr rhs) -> bool {
-  if (lhs == rhs || !lhs || !rhs) {
-    return true;
-  }
-
-  llvm::SmallVector<mlir::ktdf_arch::ResourceSpecAttr> buffer;
-  buffer.reserve(lhs.getValue().size() + rhs.getValue().size());
-  llvm::append_range(buffer, lhs.getValue());
-  const auto split = buffer.size();
-  llvm::append_range(buffer, rhs.getValue());
-  return setEqual({buffer.data(), buffer.data() + split},
-                  {buffer.data() + split, buffer.end()});
 }
 
 struct FuseLinalgPass : public impl::FuseLinalgPassBase<FuseLinalgPass> {
@@ -141,40 +244,12 @@ struct FuseLinalgPass : public impl::FuseLinalgPassBase<FuseLinalgPass> {
     getOperation()->walk(collect);
 
     mlir::RewritePatternSet patterns(&getContext());
-
-    llvm::DenseSet<mlir::Location> fused_locs;
-    const auto control_fusion = [&](mlir::OpOperand* through) -> bool {
-      auto* const producer = through->get().getDefiningOp();
-      auto* const consumer = through->getOwner();
-      if (!candidates.contains(producer)) {
-        // We know that the consumer is a candidate, but the producer might not
-        // be, and then we're not allowed to fuse with it.
-        return false;
-      }
-
-      const auto producer_mapping =
-          mlir::ktdf_arch::Mappable::getMapsTo(producer);
-      const auto consumer_mapping =
-          mlir::ktdf_arch::Mappable::getMapsTo(consumer);
-      // Only fuse ops that have compatible mappings.
-      if (!areCompatible(producer_mapping, consumer_mapping)) {
-        return false;
-      }
-
-      // Annotate the mapping in the op location, so that we will be able to
-      // recover it after fusion. We will undo this if the op is not fused.
-      if (const auto fused_mapping =
-              consumer_mapping ? consumer_mapping : producer_mapping;
-          fused_mapping) {
-        const auto fused_loc = mlir::FusedLoc::get(
-            consumer->getContext(), {consumer->getLoc()}, fused_mapping);
-        fused_locs.insert(fused_loc);
-        consumer->setLoc(fused_loc);
-      }
-      return true;
-    };
-    mlir::linalg::populateElementwiseOpsFusionPatterns(patterns,
-                                                       control_fusion);
+    patterns.add<RemoveOutsDependency>(patterns.getContext());
+    patterns.add<FuseElementwiseOps>(
+        patterns.getContext(), [&](mlir::OpOperand* through) -> bool {
+          auto* const producer = through->get().getDefiningOp();
+          return candidates.contains(producer);
+        });
 
     // Fuse as many of the candidates as possible.
     auto changed = false;
@@ -187,26 +262,6 @@ struct FuseLinalgPass : public impl::FuseLinalgPassBase<FuseLinalgPass> {
                                                    rewrite_config, &changed))) {
       signalPassFailure();
     }
-    if (!changed) {
-      markAllAnalysesPreserved();
-    }
-
-    // Restore the locations and update the fused op's mapping.
-    const auto fix = [&](mlir::linalg::GenericOp generic) {
-      if (!fused_locs.contains(generic.getLoc())) {
-        return;
-      }
-      const auto loc =
-          llvm::cast<mlir::FusedLocWith<mlir::ktdf_arch::MapsToAttr>>(
-              generic.getLoc());
-      generic->setLoc(loc.getLocations()[0]);
-
-      if (!candidates.contains(generic)) {
-        std::ignore =
-            mlir::ktdf_arch::Mappable::setMapsTo(generic, loc.getMetadata());
-      }
-    };
-    getOperation()->walk(fix);
   }
 };
 
